94128cbb3af08af3c24d722d6a46a64ec5339f4a
HuyTran <quanghuy2621998@gmail.com>
2020-06-18 11:30:26 +0700

    initial setup

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, Button } from 'react-native';
 
 export default function App() {
+  const handlePress=()=>console.log("Click me !");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello React Native aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!
+       </Text>
+       {/* <Image source={require("./assets/icon.png")}/> */}
+       <Image 
+        blurRadius={10}
+        source={{
+         width:200,
+         height:200,
+         uri:"https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF-970-80.jpg"}}
+       />
+       <Button
+        color="orange"
+        title="Click me !"
+        onPress={()=>alert('Button tapped !')}
+       />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffa',
     alignItems: 'center',
     justifyContent: 'center',
   },
